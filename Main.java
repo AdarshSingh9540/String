ab07902f78a260aca6e07269b1d1cfc009ef1339
AdarshSingh9540 <131537713+AdarshSingh9540@users.noreply.github.com>
2023-07-26 23:03:55 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String a = "adarsh";
+        String b= "adarsh";
+
+//        System.out.println(a);
+//        System.out.println(b);
+//        a="singh";
+//        System.out.println(a);
+////        System.out.println(b);
+//        System.out.println(a==b);
+
+//        String name1 = new String("adarsh");
+//        String name2 = new String("adarsh");
+//        System.out.println(name1==name2);
+//        System.out.println(name1.equals((name2)));
+//        System.out.println(name1.charAt(3));
+//        System.out.printf("my name is %s %s",a,b);
+
+        System.out.printf("pie : %.3f",Math.PI);
+        
+    }
+}
